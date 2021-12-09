bb0a9daa75d6bc26c7cbc83730d5fa4cfee11fd8
LordScarvius <lordscarvius@gmail.com>
2021-12-09 13:56:54 -0500

    commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.Drive;
 import frc.robot.subsystems.Chain;
 import frc.robot.subsystems.Drivetrain;
@@ -23,10 +24,13 @@ public class RobotContainer
 {
   // The robot's subsystems and commands are defined here...
   private final Drivetrain m_drive = new Drivetrain();
-  private final Intake intake = new Intake();
-  private final Chain chain = new Chain();
+  private final Intake m_intake = new Intake();
+  private final Chain m_chain = new Chain();
 
-  private final Joystick gamepad = new Joystick(Constants.GAMEPAD);
+  private final Joystick left = new Joystick(0);
+  private final Joystick right = new Joystick(1);
+
+  private final JoystickButton intakeBtn, chainLiftBtn, chainLowerBtn;
    
   private final Drive drive = new Drive(m_drive, gamepad.getRawAxis(0), gamepad.getRawAxis(1)); 
 
@@ -48,7 +52,13 @@ public class RobotContainer
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.  command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() 
+  {
+    intakeBtn = new JoystickButton(left, 0);
+    intakeBtn.whenPressed(new InstantCommand(() -> m_intake.runIntake(-0.5));
+
+    )
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
