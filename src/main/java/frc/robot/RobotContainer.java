9c9ee355970a9ab874c1c020cb33dad2c601695c
LordScarvius <lordscarvius@gmail.com>
2021-12-16 15:14:34 -0500

    uncommented more

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,10 +12,10 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.Drive;
-// import frc.robot.subsystems.Chain;
+import frc.robot.subsystems.Chain;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.EndgameLatch;
-// import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Intake;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -27,8 +27,8 @@ public class RobotContainer
 {
   // The robot's subsystems and commands are defined here...
   private final Drivetrain m_drive = new Drivetrain();
-  // private final Intake m_intake = new Intake();
-  // private final Chain m_chain = new Chain();
+  private final Intake m_intake = new Intake();
+  private final Chain m_chain = new Chain();
   private final EndgameLatch m_latch = new EndgameLatch();
 
   private final Joystick left = new Joystick(0);
@@ -38,10 +38,6 @@ public class RobotContainer
    
   private final Drive drive = new Drive(m_drive, -left.getRawAxis(1), right.getRawAxis(1)); 
 
-
-
-
-  // private final ExampleCommand m_autoCommand = new ExampleCommand();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() { 
@@ -59,16 +55,16 @@ public class RobotContainer
   private void configureButtonBindings() 
   {
     intakeIn = new JoystickButton(left, 0);
-    //   intakeIn.whileHeld(new StartEndCommand(() -> m_intake.runIntake(-0.25), () -> m_intake.runIntake(0), m_intake));
+      intakeIn.whileHeld(new StartEndCommand(() -> m_intake.runIntake(-0.25), () -> m_intake.runIntake(0), m_intake));
 
-    // intakeOut = new JoystickButton(right, 14);
-    //   intakeOut.whileHeld(new StartEndCommand(() -> m_intake.runIntake(1), () -> m_intake.runIntake(0), m_intake));
+    intakeOut = new JoystickButton(right, 14);
+      intakeOut.whileHeld(new StartEndCommand(() -> m_intake.runIntake(1), () -> m_intake.runIntake(0), m_intake));
 
-    // chainLift = new JoystickButton(right, 10);
-    //   chainLift.whileHeld(new StartEndCommand(() -> m_chain.runChain(1), () -> m_chain.runChain(0), m_chain));
+    chainLift = new JoystickButton(right, 10);
+      chainLift.whileHeld(new StartEndCommand(() -> m_chain.runChain(1), () -> m_chain.runChain(0), m_chain));
 
-    // chainLower = new JoystickButton(right, 15);
-    //   chainLower.whileHeld(new StartEndCommand(() -> m_chain.runChain(-1), () -> m_chain.runChain(0), m_chain));
+    chainLower = new JoystickButton(right, 15);
+      chainLower.whileHeld(new StartEndCommand(() -> m_chain.runChain(-1), () -> m_chain.runChain(0), m_chain));
 
     endgameLatch = new JoystickButton(right, 3);
       endgameLatch.whenPressed(new InstantCommand(() -> m_latch.toggle(), m_latch));
